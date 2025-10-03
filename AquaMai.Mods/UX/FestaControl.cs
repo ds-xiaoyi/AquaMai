e304e21c9853f56a22c7c96cfed60f6482dfdf1a
Clansty <i@gao4.pw>
2025-10-03 16:14:19 +0800

    [+] FestaControl

---
+++ AquaMai.Mods/UX/FestaControl.cs
@@ -0,0 +1,25 @@
+using AquaMai.Config.Attributes;
+using AquaMai.Core.Attributes;
+using HarmonyLib;
+using Manager;
+
+namespace AquaMai.Mods.UX;
+
+[ConfigSection(
+    zh: "控制 “Festa” 模式 UI 的显示。如不开启，则不会更改设置，由 Event 或者服务器控制",
+    en: "Control the display of “Festa” mode UI. If not enabled, the settings will not be changed, and Event or server will control it")]
+[EnableGameVersion(26000)]
+public static class FestaControl
+{
+    [ConfigEntry(
+        zh: "是否显示 “Festa” 模式 UI",
+        en: "Whether to display “Festa” mode UI")]
+    public static readonly bool isFesta = false;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(FestaManager), "Initalize")]
+    public static void Hook(FestaManager __instance)
+    {
+        Traverse.Create(__instance).Property<bool>("isOpenFesta").Value = isFesta;
+    }
+}

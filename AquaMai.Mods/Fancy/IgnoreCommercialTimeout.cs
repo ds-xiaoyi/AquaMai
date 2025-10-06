92c4ed9b318f2614a73badac75666720d0b9bd19
WUGqnwvMQPzl <36147447+WUGqnwvMQPzl@users.noreply.github.com>
2025-10-06 13:30:30 +0800

    [+] 允许忽略闲置时视频广告画面的 5 分钟总时长限制 (#64)
    * [+] IgnoreCommercialTimeout
    
    * [F] oops

---
+++ AquaMai.Mods/Fancy/IgnoreCommercialTimeout.cs
@@ -0,0 +1,18 @@
+﻿using AquaMai.Config.Attributes;
+using HarmonyLib;
+using Process.AdvertiseCommercial;
+
+namespace AquaMai.Mods.Fancy;
+
+[ConfigSection(
+    en: "Ignores 5 mins total timeout on commercial screen.",
+    zh: "忽略闲置时的视频广告画面的5分钟总时长限制")]
+public class IgnoreCommercialTimeout
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(AdvertiseCommercialProcess), "CheckTotalTimeOut")]
+    public static bool PreCheckTotalTimeOut()
+    {
+        return false;
+    }
+}
